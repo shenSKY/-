c46754d1e0c52d99014d22efb80dfcaed1cb4928
shenSKY <sk610037874@gmail.com>
2017-05-03 17:52:02 +0800

    简易轮播
    可以手动进行滑动的轮播

---
+++ 轮播Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  轮播Demo
+//
+//  Created by 沈凯 on 2017/5/3.
+//  Copyright © 2017年 神SKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 轮播Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  轮播Demo
+//
+//  Created by 沈凯 on 2017/5/3.
+//  Copyright © 2017年 神SKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
